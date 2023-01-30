503e834dae2dc6440dc54624d70263ea27da8ad9
YeeshuPushparag <pushparagyeeshu@gmail.com>
2023-01-30 05:09:53 -0800

    changed hide secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!$uskpz8@gwyl6429-n_@w3cat1-f#zox&ceupobus4qt&eac'
+SECRET_KEY = 'Django-Secret-Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
